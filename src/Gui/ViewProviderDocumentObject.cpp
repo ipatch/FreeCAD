38581ab27461b6bbd869c1cd45e378455dba9861
Zheng, Lei <realthunder.dev@gmail.com>
2021-10-25 16:33:38 +0800

    App/Gui: fix getFullName() handling when not attached to document

--- src/Gui/ViewProviderDocumentObject.cpp
+++ src/Gui/ViewProviderDocumentObject.cpp
@@ -747,8 +747,6 @@ ViewProviderDocumentObject *ViewProviderDocumentObject::getLinkedViewProvider(
 }
 
 std::string ViewProviderDocumentObject::getFullName(bool python) const {
-    if (!isAttachedToDocument())
-        return "?";
     if (pcObject)
         return pcObject->getFullName(python) + ".ViewObject";
     return std::string(python?"None":"?");
