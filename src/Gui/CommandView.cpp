00bacd3fb5ef5fa3ea08b5c9d5b7cf507163ceda
Zheng, Lei <realthunder.dev@gmail.com>
2021-12-10 17:52:44 +0800

    Gui: change shortcut for Std_TreeToggleShowHidden
    Because it conflicts with 'Save as'

--- src/Gui/CommandView.cpp
+++ src/Gui/CommandView.cpp
@@ -3755,7 +3755,7 @@ StdTreeToggleShowHidden::StdTreeToggleShowHidden()
 	sStatusTip      = sToolTipText;
 	sWhatsThis      = "Std_TreeToggleShowHidden";
 	sPixmap         = "tree-show-hidden";
-	sAccel          = "Ctrl+Shift+S";
+	sAccel          = "Shift+Alt+S";
 	eType           = 0;
 }
 
