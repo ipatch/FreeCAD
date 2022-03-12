65cbcd02e4e46f8f05780a99ac1a30eece74a184
Zheng, Lei <realthunder.dev@gmail.com>
2022-03-12 15:33:39 +0800

    Gui: fix Customize -> Keyboard shortcut priroity list
    Add API Command::initAction() to force create action for all commands
    with shortcut in order to register with ShortcutManager to obtain a
    complete list of actions with the same shortcut.

--- src/Gui/Command.cpp
+++ src/Gui/Command.cpp
@@ -268,7 +268,7 @@ bool Command::isViewOfType(Base::Type t) const
         return false;
 }
 
-void Command::addTo(QWidget *pcWidget)
+void Command::initAction()
 {
     if (!_pcAction) {
         BitmapCacheContext ctx(getName());
@@ -281,7 +281,11 @@ void Command::addTo(QWidget *pcWidget)
         setShortcut(ShortcutManager::instance()->getShortcut(getName(), getAccel()));
         testActive();
     }
+}
 
+void Command::addTo(QWidget *pcWidget)
+{
+    initAction();
     _pcAction->addTo(pcWidget);
 }
 
@@ -293,17 +297,7 @@ void Command::addToGroup(ActionGroup* group, bool checkable)
 
 void Command::addToGroup(ActionGroup* group)
 {
-    if (!_pcAction) {
-        BitmapCacheContext ctx(getName());
-        _pcAction = createAction();
-#ifdef FC_DEBUG
-        // Accelerator conflict can now be dynamically resolved in ShortcutManager
-        //
-        // printConflictingAccelerators();
-#endif
-        setShortcut(ShortcutManager::instance()->getShortcut(getName(), getAccel()));
-        testActive();
-    }
+    initAction();
     group->addAction(_pcAction->findChild<QAction*>());
 }
 
