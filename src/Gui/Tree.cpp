67a8c4972cae20ebc2cd4d1e84000c5e23d56048
Zheng, Lei <realthunder.dev@gmail.com>
2022-03-21 22:24:07 +0800

    Gui: fix regression of tree icon restore
    Fixes realthunder/FreeCAD_Assembly3#296

--- src/Gui/Tree.cpp
+++ src/Gui/Tree.cpp
@@ -673,8 +673,6 @@ public:
 
 //--------------------------------------------------------------------------
 
-static int _TreeIconSize;
-
 /*[[[cog
 import TreeParams
 TreeParams.define()
@@ -3296,22 +3294,31 @@ void TreeWidget::setItemSpacing(int spacing)
     TreeParams::setItemSpacing(spacing);
 }
 
+static int &_TreeIconSize()
+{
+    static int treeIconSize = -1;
+
+    if (treeIconSize < 0)
+        treeIconSize = TreeParams::getIconSize();
+    return treeIconSize;
+}
+
 int TreeWidget::iconHeight() const
 {
-    return _TreeIconSize;
+    return _TreeIconSize();
 }
 
 void TreeWidget::setIconHeight(int height)
 {
-    if (_TreeIconSize == height)
+    if (_TreeIconSize() == height)
         return;
 
-    _TreeIconSize = height;
-    if (_TreeIconSize <= 0)
-        _TreeIconSize = std::max(10, iconSize());
+    _TreeIconSize() = height;
+    if (_TreeIconSize() <= 0)
+        _TreeIconSize() = std::max(10, iconSize());
 
     for(auto tree : Instances)
-        tree->setIconSize(QSize(_TreeIconSize, _TreeIconSize));
+        tree->setIconSize(QSize(_TreeIconSize(), _TreeIconSize()));
 }
 
 int TreeWidget::iconSize() {
@@ -3323,8 +3330,8 @@ int TreeWidget::iconSize() {
         else
             defaultSize = QApplication::style()->pixelMetric(QStyle::PM_SmallIconSize);
     }
-    if (_TreeIconSize > 0)
-        return std::max(10, _TreeIconSize);
+    if (_TreeIconSize() > 0)
+        return std::max(10, _TreeIconSize());
     return defaultSize;
 }
 
