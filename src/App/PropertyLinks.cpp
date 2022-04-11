f149a78c6ad38e7ba840c350e21feab1eeb7299d
Zheng, Lei <realthunder.dev@gmail.com>
2022-04-11 14:16:03 +0800

    App: fix crash on replacing object
    Normally through drag and drop
    
    Fixes realthunder/FreeCAD_Assembly3#1198

--- src/App/PropertyLinks.cpp
+++ src/App/PropertyLinks.cpp
@@ -481,6 +481,8 @@ PropertyLinkBase::tryReplaceLink(const PropertyContainer *owner, DocumentObject 
 {
     std::pair<DocumentObject*, std::string> res;
     res.first = 0;
+    if (!obj)
+        return res;
 
     if(oldObj == obj) {
         if(owner == parent) {
@@ -559,6 +561,8 @@ PropertyLinkBase::tryReplaceLinkSubs(const PropertyContainer *owner,
 {
     std::pair<DocumentObject*,std::vector<std::string> > res;
     res.first = 0;
+    if (!obj)
+        return res;
 
     auto r = tryReplaceLink(owner,obj,parent,oldObj,newObj);
     if(r.first) {
